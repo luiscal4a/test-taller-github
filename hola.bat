821a0e49507e8962a96c4565fc33aebcba73d249
XPi2 <uxiopineiro@gmail.com>
2020-10-17 18:19:13 +0200

    Limpieza

--- hola.bat
+++ hola.bat
@@ -2,10 +2,7 @@
 ECHO Este es mi primer script
 ECHO Quieres una taza de te?
 ECHO Pulsa S para Si
-ECHO Pulsa N para no
 CHOICE /c SN /m "Si o NO"
-ECHO "Me gustan los trenes"
-ECHO Hace calor
 CLS
 ECHO He limpiado la pantalla
 ECHO %DATE%
